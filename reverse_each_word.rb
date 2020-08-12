3a53b188ebcf40c5fe83e1f28c7029f57d7b9124
Will Gonzalez <will.colocho@gmail.com>
2020-08-12 04:28:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   rev_array = []
-  array.collect do |string|
+  string.collect do |string|
   rev_array << array.reverse
   end
   rev_array.join(" ")
